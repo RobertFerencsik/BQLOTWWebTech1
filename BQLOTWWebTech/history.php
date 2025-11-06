a482e7319b6543135096ee35d9d4fe719fcfb34f
Robert Ferencsik <robert.ferencsik@gmail.com>
2025-11-06 21:19:43 +0100

    refactor: Move page files into the root

--- BQLOTWWebTech/history.php
+++ BQLOTWWebTech/history.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Főoldal - BQLOTW WebTech</title>
-    <link rel="icon" type="image/x-icon" href="../media/images/favicon.png">
-    <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="icon" type="image/x-icon" href="src/media/images/favicon.png">
+    <link rel="stylesheet" href="./src/styles/styles.css">
 </head>
 <body>
-    <?php include('../components/header.php'); ?>
+    <?php include('./src/components/header.php'); ?>
 
     <main>
     </main>
 
-    <?php include('../components/footer.php'); ?>
+    <?php include('./src/components/footer.php'); ?>
 
     <script src="script.js"></script>
 </body>
--- BQLOTWWebTech/src/components/header.php
+++ BQLOTWWebTech/src/components/header.php
@@ -1,6 +1,10 @@
 <header class="header">
-  <a href="#default" class="logo">LSTM by Robert</a>
+  <a href="index.php" class="logo">LSTM by Robert</a>
   <div class="header-right">
-    <a class="active" href="#home">Home</a>
+    <a class="active" href="index.php">Home</a>
+    <a href="history.php">History</a>
+    <a href="architecture.php">Architecture</a>
+    <a href="example.php">Example</a>
+    <a href="contact.php">Contact</a>
   </div>
 </header>
