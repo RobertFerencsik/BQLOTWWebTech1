661e6794aa071f798227546394c683cd9da66137
Robert Ferencsik <robert.ferencsik@gmail.com>
2025-11-06 18:34:59 +0100

    feat: Add landing page skeleton

---
+++ BQLOTWWebTech/index.php
@@ -0,0 +1,24 @@
+<?php 
+$current_page = 'index.php';
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Főoldal - BQLOTW WebTech</title>
+    <link rel="stylesheet" href="./src/styles/styles.css">
+</head>
+<body>
+    <?php include('./src/components/navbar.php'); ?>
+
+    <main>
+        <!-- placeholder -->
+    </main>
+
+    <?php include('./src/components/footer.php'); ?>
+
+    <script src="script.js"></script>
+</body>
+</html>
+
