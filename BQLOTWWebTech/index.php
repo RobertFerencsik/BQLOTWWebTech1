acccaa6e61c8a3e395370c8e75f366ecf7875c28
Robert Ferencsik <robert.ferencsik@gmail.com>
2025-11-06 20:34:29 +0100

    feat: Add basic header and footer as components, also add basic style and favicon

--- BQLOTWWebTech/index.php
+++ BQLOTWWebTech/index.php
@@ -1,19 +1,16 @@
-<?php 
-$current_page = 'index.php';
-?>
 <!DOCTYPE html>
 <html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Főoldal - BQLOTW WebTech</title>
+    <link rel="icon" type="image/x-icon" href="src/media/images/favicon.png">
     <link rel="stylesheet" href="./src/styles/styles.css">
 </head>
 <body>
     <?php include('./src/components/navbar.php'); ?>
 
     <main>
-        <!-- placeholder -->
     </main>
 
     <?php include('./src/components/footer.php'); ?>
